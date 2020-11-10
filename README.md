f1172280d55cc27f4db7ddbb3f91ea13d5d1a223
Riko Gercke <riko.gercke@gmail.com>
2020-11-10 21:53:29 +0100

    skipping and cleanup
    added option to skip finished downloads and a cleanup for tempfiles

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # About
-A southpark downloader which works with the new soutpark.de and uses [youtube-dlc](https://github.com/blackjack4494/yt-dlc) and [ffmpeg](https://github.com/FFmpeg/FFmpeg) to download all episodes in 1080p
+A southpark downloader which works with the new southpark.de and uses [youtube-dlc](https://github.com/blackjack4494/yt-dlc) and [ffmpeg](https://github.com/FFmpeg/FFmpeg) to download all episodes in 1080p
 
 # Requirements
  - [Python 3](https://www.python.org/downloads/) installed
@@ -7,7 +7,7 @@ A southpark downloader which works with the new soutpark.de and uses [youtube-dl
  - run `pip3 install -r requirements.txt`
 
 ## Run
- 1. open a shell and run the python script with `python3 run.py`
+ 1. open a shell and run the python script with `python3 run.py` or `python3 run.py -y ` to skip finished downloads
 
 ### Progress
 The script saves the downloaded files in the `temp` directory and copys the progressed finished files into the `output` folder. While its running, the youtube-dlc and ffmpeg output will be shown.
--- main.py
+++ main.py
@@ -4,35 +4,59 @@ import subprocess
 from pathlib import Path
 from os import listdir
 from os.path import isfile, join
-from shutil import copyfile
+from shutil import copyfile, rmtree
 from pathvalidate import sanitize_filename
-null = None
-episodes = []
+import sys
+
 souhtparkapiurl = 'https://www.southpark.de/api/episodes/1/18'
+
 customurl = True
 debug = True
+skip = False
+
+episodes = []
+null = None
+
 def download (url, number, title):
-    #print(episode)
     temppath = "./temp/"+number
     outputpath = "./output/"
-    outputfile = sanitize_filename(number +' - ' + title +'.mp4')
+    outputname = sanitize_filename(number +' - ' + title)
+    outputfile = outputname +'.mp4'
+
+    Path(outputpath).mkdir(parents=True, exist_ok=True)
+    existingfiles = [f for f in listdir(outputpath) if isfile(join(outputpath, f))]
+
+    if skip:
+        for file in existingfiles:
+            if number in file:
+                print('file with name "' + file + '" in ' + outputpath + ' found. skipping: ' + title)
+                return
+
     Path(temppath).mkdir(parents=True, exist_ok=True)
+
     subprocess.run(['youtube-dlc.exe','--restrict-filenames', url], cwd=temppath)
     onlyfiles = [f for f in listdir(temppath) if isfile(join(temppath, f))]
+
     with open(temppath + '/parts.txt', 'w') as f:
         for part in onlyfiles:
             if part != 'parts.txt':
                 if part != outputfile:
                     f.write("file '"+part+"'\n")
+
     subprocess.run(['ffmpeg.exe', '-y', '-f', 'concat', '-safe', '0', '-i', 'parts.txt', '-c', 'copy', outputfile], cwd=temppath)
-    Path(outputpath).mkdir(parents=True, exist_ok=True)
     copyfile(temppath + '/' + outputfile, outputpath + outputfile)
+    rmtree(temppath)
+
+if (len(sys.argv) > 1):
+    if sys.argv[1] == '-y':
+        print('enabled skipping of finished downloads')
+        skip = True
 
 with urllib.request.urlopen(souhtparkapiurl) as url:
     response = json.loads(url.read().decode())
-    loadmore = response['loadMore']
+    running = True
 
-    while loadmore != null:
+    while running == True:
         if customurl == True:
             with urllib.request.urlopen(souhtparkapiurl) as url:
                 response = json.loads(url.read().decode())
@@ -47,13 +71,13 @@ with urllib.request.urlopen(souhtparkapiurl) as url:
             description = episode['meta']['description']
             url = episode['url']
             url = 'https://www.southpark.de' + url
-            #print(number + ' ' + title + ': ' + description + ' -- ' + url)
             episode_set = {number: [{"title": title}, {'description': description}, {'url': url}]}
             download(url, number,title)
             episodes.append(episode_set)
 
         if response['loadMore'] == null:
-            loadmore = null
+            running = False
+            print('finished downloading ' + len(episodes))
 
 
 with open('episodes.json', 'w', encoding='utf-8') as f:
